3c12f634ae6c3d4a761e1b0604a970f9cdf0d561
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-04-20 15:46:56 +0200

    c0.0 - initializing Spring Boot, Gradle, etc.

---
+++ src/main/java/com/example/habitproject/HabitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.habitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/habitproject/HabitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.habitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
