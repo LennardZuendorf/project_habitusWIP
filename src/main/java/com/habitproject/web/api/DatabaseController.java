958ee537b2dc71b3ed4c4f788a11e9770396788b
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-05-05 09:49:34 +0200

    c0.8.1 - additional datasource configurations

---
+++ src/main/java/com/habitproject/web/api/DatabaseController.java
@@ -0,0 +1,22 @@
+package com.habitproject.web.api;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//solution found on HTW WI Slack
+
+@Controller
+public class DatabaseController {
+    final
+    Environment environment;
+
+    public DatabaseController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("/print")
+    public String printJDBCCreds(){
+        return environment.getProperty("JDBC_DATABASE_URL");
+    }
+}
