589d9c7997bebcb54850fdd44934a27c608563b0
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-05-09 17:06:03 +0200

    c17.0 - implementing methods for habits, changing HabitRepository accordingly

--- src/main/java/com/habitproject/persistence/HabitRepository.java
+++ src/main/java/com/habitproject/persistence/HabitRepository.java
@@ -1,10 +1,16 @@
 package com.habitproject.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface HabitRepository extends JpaRepository<HabitEntity, Long> {
 
-    List<HabitEntity> findAllByUserId(Long UserId);
+    List<HabitEntity> findAllByUserId(Long userId);
+
+    @Modifying
+    @Query("update HabitEntity h set h.frequency =?2, h.quantity=?3, h.tag=?4 where h.id=?1")
+    void updateHabitByID(Long id, String tag, HabitQuantity quantity, Integer frequency);
 }
--- src/main/java/com/habitproject/service/AppServiceImpl.java
+++ src/main/java/com/habitproject/service/AppServiceImpl.java
@@ -1,34 +1,81 @@
 package com.habitproject.service;
 
-import com.habitproject.persistence.GoalEntity;
 import com.habitproject.persistence.HabitEntity;
+import com.habitproject.persistence.HabitQuantity;
+import com.habitproject.persistence.HabitRepository;
 
 import java.util.List;
 
 public class AppServiceImpl implements AppService {
 
-    @Override
-    public HabitEntity putHabit() {
-        return null;
+    private final HabitRepository habitRepository;
+    public AppServiceImpl(HabitRepository repository) {
+        this.habitRepository = repository;
     }
 
+    /**
+     * saving a new habit to database
+     * @param tag - a tag for the habit (name)
+     * @param quantity - quantity of the habit
+     * @param frequency - the frequency a habit should be reached
+     * @param userId - id of the user the Habit belongs to
+     * @return newly created habit
+     */
     @Override
-    public HabitEntity getHabit() {
-        return null;
+    public HabitEntity putHabit(String tag, HabitQuantity quantity, Integer frequency, Long userId) {
+        HabitEntity newHabit = new HabitEntity(tag, quantity, frequency, userId);
+        habitRepository.saveAndFlush(newHabit);
+        return newHabit;
     }
 
+    /**
+     * getting one HabitEntity by id
+     * @param id - the habit id to select
+     * @return selected HabitEntity
+     */
     @Override
-    public List<HabitEntity> getAllHabit() {
-        return null;
+    public HabitEntity getHabit(Long id) {
+        return habitRepository.getOne(id);
     }
 
+    /**
+     * getting a list of all habits by UserID
+     * @param userId - the user ID to be selected by
+     * @return list of HabitEntities
+     */
     @Override
-    public HabitEntity updateHabit() {
-        return null;
+    public List<HabitEntity> getAllHabit(Long userId) {
+        return habitRepository.findAllByUserId(userId);
     }
 
+    /**
+     * updating a HabitEntity in the database
+     * @param id - id of the habit to update
+     * @param tag - a tag for the habit (name)
+     * @param quantity - quantity of the habit
+     * @param frequency - the frequency a habit should be reached
+     * @return updated habit
+     */
     @Override
-    public HabitEntity deleteHabit() {
-        return null;
+    public HabitEntity updateHabit(Long id, String tag, HabitQuantity quantity, Integer frequency) {
+        habitRepository.updateHabitByID(id, tag, quantity,frequency);
+        habitRepository.flush();
+        return habitRepository.getOne(id);
     }
+
+    /**
+     * API call for deleting a habit (HabitEntity)
+     * @param id - id of the habit that should be deleted
+     * @return deleted habit
+     */
+    @Override
+    public HabitEntity deleteHabit(Long id) {
+        var output = habitRepository.getOne(id);
+        habitRepository.delete(output);
+        habitRepository.flush();
+        return output;
+
+    }
+
+
 }
