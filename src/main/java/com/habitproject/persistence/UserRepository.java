1f5d0631fb75ea221b95a72a3f4d547455b6ace7
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-05-10 16:53:25 +0200

    c32.0 - implementing UserRepository, UserRequestModel for usage with service

---
+++ src/main/java/com/habitproject/persistence/UserRepository.java
@@ -0,0 +1,12 @@
+package com.habitproject.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<UserAccountEntity, Long> {
+    UserAccountEntity findFirstByUid(Long uid);
+}
