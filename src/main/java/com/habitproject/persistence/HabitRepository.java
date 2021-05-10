01228828f14a7389425f4f3cdcac0497196b5ef4
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-05-10 16:51:21 +0200

    c29.3 - cleaning up HabitRepository

--- src/main/java/com/habitproject/persistence/HabitRepository.java
+++ src/main/java/com/habitproject/persistence/HabitRepository.java
@@ -11,8 +11,4 @@ public interface HabitRepository extends JpaRepository<HabitEntity, Long> {
 
     List<HabitEntity> findAllByUid(Long uid);
     HabitEntity findFirstByHid(Long hid);
-
-    @Modifying
-    @Query("update HabitEntity h set h.tag=?2, h.frequency=?3, h.quantity =?4 where h.hid=?1")
-    void updateHabitByHid(@Param(value= "hid") long hid, @Param(value="tag")String tag, @Param(value="frequency") HabitFrequency frequency, @Param(value="quantity")Integer quantity);
 }
