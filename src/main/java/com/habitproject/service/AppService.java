840b80ce164a9cdab70e8c3aa19117c07b21c68e
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-05-10 19:17:56 +0200

    c35.0 - implementing service for GoalEntity Endpoints

--- src/main/java/com/habitproject/service/AppService.java
+++ src/main/java/com/habitproject/service/AppService.java
@@ -1,6 +1,8 @@
 package com.habitproject.service;
 
+import com.habitproject.persistence.GoalEntity;
 import com.habitproject.persistence.HabitEntity;
+import com.habitproject.web.api.GoalRequestModel;
 import com.habitproject.web.api.HabitRequestModel;
 import com.habitproject.web.api.UserRequestModel;
 import org.springframework.http.HttpStatus;
@@ -15,8 +17,15 @@ public interface AppService {
     HttpStatus putHabit(Long hid, HabitRequestModel requestBody);
     HttpStatus deleteHabit(Long hid);
 
+    HttpStatus postGoal(GoalRequestModel requestBody);
+    GoalEntity getGoal(Long gid);
+    List<GoalEntity> getAllGoal(Long uid);
+    HttpStatus putGoal(Long gid, GoalRequestModel requestBody);
+    HttpStatus deleteGoal(Long gid);
+
     HttpStatus postUser(UserRequestModel requestBody);
     HttpStatus putUser(Long uid, UserRequestModel requestBody);
     HttpStatus deleteUser(Long uid);
+
 
 }
--- src/main/java/com/habitproject/service/AppServiceImpl.java
+++ src/main/java/com/habitproject/service/AppServiceImpl.java
@@ -1,9 +1,7 @@
 package com.habitproject.service;
 
-import com.habitproject.persistence.HabitEntity;
-import com.habitproject.persistence.HabitRepository;
-import com.habitproject.persistence.UserAccountEntity;
-import com.habitproject.persistence.UserRepository;
+import com.habitproject.persistence.*;
+import com.habitproject.web.api.GoalRequestModel;
 import com.habitproject.web.api.HabitRequestModel;
 import com.habitproject.web.api.UserRequestModel;
 import org.springframework.http.HttpStatus;
@@ -20,9 +18,11 @@ public class AppServiceImpl implements AppService {
 
     private final HabitRepository habitRepository;
     private final UserRepository userRepository;
-    public AppServiceImpl(HabitRepository repository, UserRepository userRepository) {
+    private final GoalRepository goalRepository;
+    public AppServiceImpl(HabitRepository repository, UserRepository userRepository, GoalRepository goalRepository) {
         this.habitRepository = repository;
         this.userRepository = userRepository;
+        this.goalRepository = goalRepository;
     }
 
     //services for habit API endpoints
@@ -88,6 +88,72 @@ public class AppServiceImpl implements AppService {
         if (habitRepository.existsById(hid)) {
             habitRepository.delete(habitRepository.getOne(hid));
             habitRepository.flush();
+            return HttpStatus.OK;
+        } else return HttpStatus.NO_CONTENT;
+    }
+
+    //services for goal API endpoints
+    /**
+     * saving a new goal to database
+     * @param requestBody - all of GoalEntity params
+     * @return http status code
+     */
+    @Override
+    public HttpStatus postGoal(GoalRequestModel requestBody) {
+        GoalEntity newGoal = new GoalEntity(requestBody.getTag(), requestBody.getMeasure(), requestBody.getCurrentAmount(), requestBody.getTotalAmount(), requestBody.getUid());
+        goalRepository.saveAndFlush(newGoal);
+        return HttpStatus.CREATED;
+    }
+
+    /**
+     * getting one GoalEntity by id
+     * @param gid - the goal id to select by
+     * @return selected GoalEntity
+     */
+    @Override
+    public GoalEntity getGoal(Long gid) {
+        return goalRepository.findFirstByGid(gid);
+    }
+
+    /**
+     * getting a list of all goals by UserID
+     * @param uid - the user ID to be selected by
+     * @return list of GoalEntities
+     */
+    @Override
+    public List<GoalEntity> getAllGoal(Long uid) {
+        return goalRepository.findAllByUid(uid);
+    }
+
+    /**
+     * updating a HabitEntity in the database
+     * @param gid          - id of the habit to update
+     * @param requestBody - all of HabitEntity params
+     * @return Http status code
+     */
+    @Override
+    public HttpStatus putGoal(Long gid, GoalRequestModel requestBody) {
+        if(goalRepository.existsById(gid)){
+            GoalEntity goalEntity = goalRepository.findFirstByGid(gid);
+            goalEntity.setTag(requestBody.getTag());
+            goalEntity.setMeasure(requestBody.getMeasure());
+            goalEntity.setCurrentAmount(requestBody.getCurrentAmount());
+            goalEntity.setTotalAmount(requestBody.getTotalAmount());
+            goalRepository.saveAndFlush(goalEntity);
+            return HttpStatus.ACCEPTED;
+        }else return HttpStatus.NO_CONTENT;
+    }
+
+    /**
+     * API call for deleting a goal (GoalEntity)
+     * @param gid - id of the goal that should be deleted
+     * @return Http status code
+     */
+    @Override
+    public HttpStatus deleteGoal(Long gid) {
+        if (goalRepository.existsById(gid)) {
+            goalRepository.delete(goalRepository.getOne(gid));
+            goalRepository.flush();
             return HttpStatus.OK;
         } else return HttpStatus.NO_CONTENT;
     }
