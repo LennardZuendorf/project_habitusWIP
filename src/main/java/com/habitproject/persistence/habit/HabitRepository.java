766e3e38ef6d1d9c84206e769a7cd6d5f45d126b
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-05-11 17:12:53 +0200

    c40.0 - implemented existsbyUid method

--- src/main/java/com/habitproject/persistence/habit/HabitRepository.java
+++ src/main/java/com/habitproject/persistence/habit/HabitRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface HabitRepository extends JpaRepository<HabitEntity, Long> {
-
     List<HabitEntity> findAllByUid(Long uid);
     HabitEntity findFirstByHid(Long hid);
+    Boolean existsByUid(Long uid);
 }
