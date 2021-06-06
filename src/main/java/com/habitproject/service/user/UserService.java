4b5d94d03483a0a5940a7f312f05ff98a548a7eb
LennardZuendorf <3O93a4dkSY5miins%4rC>
2021-06-06 19:15:31 +0200

    c91.0 - removing user classes

--- src/main/java/com/habitproject/service/user/UserService.java
+++ src/main/java/com/habitproject/service/user/UserService.java
@@ -1,14 +1,13 @@
 package com.habitproject.service.user;
 
 import com.habitproject.web.user.UserRequestModel;
-import com.habitproject.web.user.UserStatusReturn;
 import org.springframework.http.HttpStatus;
 
 public interface UserService {
 
     //UserAccountEntity services
-    UserStatusReturn postUser(UserRequestModel requestBody);
-    UserStatusReturn getUser(Long uid);
+    String postUser(UserRequestModel requestBody);
+    String getUser(Long uid);
     HttpStatus putUser(Long uid, UserRequestModel requestBody);
     HttpStatus deleteUser(Long uid);
 
