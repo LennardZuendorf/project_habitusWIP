7024545482f8d61817948538f31a70a2ab32e68a
LennardZuendorf <3O93a4dkSY5miins%4rC>
2021-06-07 20:37:57 +0200

    c92.0 - implementing js for colorMode

---
+++ src/main/resources/static/js/colorMode.js
@@ -0,0 +1,35 @@
+//example found at: https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
+
+
+// function to set a color mode and save to localstorage
+function toggleColor() {
+
+    //changing color mode
+    const element = document.body;
+    element.classList.toggle("light-mode");
+
+    //saving color mode
+    if (element.classList.contains("light-mode") === true){
+        localStorage.setItem("colorMode", "light");
+    }else localStorage.setItem("colorMode", "dark");
+
+    //changing color mode icon and color of the icon
+    const x = document.getElementById("colorModeIcon")
+
+    if(localStorage.getItem("colorMode")==="light"){
+        x.classList.remove("fa-sun");
+        x.classList.add("fa-moon");
+        x.style.color="var(--goal-color)";
+    }else{
+        x.classList.remove("fa-moon");
+        x.classList.add("fa-sun");
+        x.style.color="var(--habit-color)";
+    }
+}
+
+// set correct color mode immediately
+(function () {
+    if (localStorage.getItem('colorMode') === 'light') {
+        toggleColor();
+    }
+})();
