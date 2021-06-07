651b6c1f138470889b74a5dec9412732863d5b08
LennardZuendorf <3O93a4dkSY5miins%4rC>
2021-06-07 23:02:28 +0200

    c103.0 - adding basic code for custom login-form

--- src/main/resources/static/js/vue/LoginComponent.vue
+++ src/main/resources/static/js/vue/LoginComponent.vue
@@ -1,13 +1,46 @@
-<template>
+<!-- based on code from: https://developer.okta.com/blog/2020/05/15/vue-login, by Holger Schmitz -->
 
+<template>
+  <div>
+    <h2>Login</h2>
+    <p v-if="$route.query.redirect">
+      You need to login first.
+    </p>
+    <form @submit.prevent="login" autocomplete="off">
+      <label><input v-model="email" placeholder="email" v-focus></label>
+      <label><input v-model="pass" placeholder="password" type="password"></label><br>
+      <button type="submit">login</button>
+      <p v-if="error" class="error">Bad login information</p>
+    </form>
+  </div>
 </template>
 
 <script>
+import auth from '../auth'
+
 export default {
-  name: "login_component"
+  data () {
+    return {
+      email: '',
+      pass: '',
+      error: false
+    }
+  },
+  methods: {
+    login () {
+      auth.login(this.email, this.pass, loggedIn => {
+        if (!loggedIn) {
+          this.error = true
+        } else {
+          this.$router.replace(this.$route.query.redirect || '/')
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+  @import "src/main/resources/static/css/VueStyle.css";
+  @import "src/main/resources/static/css/HtmlStyle.css";
 </style>
