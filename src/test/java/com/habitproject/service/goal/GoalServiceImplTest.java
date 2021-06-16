b651d8ffe72da349f20941e220a661be088687dc
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-06-16 16:33:16 +0200

    c117.5 - fixing tests

--- src/test/java/com/habitproject/service/goal/GoalServiceImplTest.java
+++ src/test/java/com/habitproject/service/goal/GoalServiceImplTest.java
@@ -1,33 +1,3 @@
 package com.habitproject.service.goal;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class GoalServiceImplTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @Test
-    void postGoal() {
-    }
-
-    @Test
-    void getGoal() {
-    }
-
-    @Test
-    void getAllGoal() {
-    }
-
-    @Test
-    void putGoal() {
-    }
-
-    @Test
-    void deleteGoal() {
-    }
-}
+class GoalServiceImplTest {}
