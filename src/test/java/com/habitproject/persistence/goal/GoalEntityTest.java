923ad1cb5a0463d5197403d59255026e1f1d5231
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-06-16 17:09:57 +0200

    c117.7 - cleaning uo tests

--- src/test/java/com/habitproject/persistence/goal/GoalEntityTest.java
+++ src/test/java/com/habitproject/persistence/goal/GoalEntityTest.java
@@ -1,28 +1,4 @@
 package com.habitproject.persistence.goal;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-class GoalEntityTest {
-
-    @BeforeAll
-    void setUp(){
-
-    }
-
-
-    @Test
-    @DisplayName("")
-    void testGoalEntity(){
-        //expected
-
-
-        //actual
-
-
-        //assertion
-
-    }
-}
+class GoalEntityTest { }
 
