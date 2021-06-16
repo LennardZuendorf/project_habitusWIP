bdaf99af02e3be94e04b7201fab5147992b8104e
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-06-16 15:47:27 +0200

    c114.2 - adding basic LoginComponent.vue

--- src/main/resources/static/js/vue/LoginComponent.vue
+++ src/main/resources/static/js/vue/LoginComponent.vue
@@ -1,24 +1,43 @@
 <!-- based on code from: https://developer.okta.com/blog/2020/05/15/vue-login, by Holger Schmitz -->
 
 <template>
-  <div>
-    <h2>Login</h2>
-    <p v-if="$route.query.redirect">
-      You need to login first.
-    </p>
-    <form @submit.prevent="login" autocomplete="off">
-      <label><input v-model="email" placeholder="email" v-focus></label>
-      <label><input v-model="pass" placeholder="password" type="password"></label><br>
-      <button type="submit">login</button>
-      <p v-if="error" class="error">Bad login information</p>
-    </form>
-  </div>
+  <!-- Card -->
+  <mdb-card>
+    <mdb-card-body>
+      <form>
+        <p class="h4 text-center py-4">Sign up</p>
+        <div class="grey-text">
+          <mdb-input label="Your name" icon="user" group type="text" validate error="wrong" success="right"/>
+          <mdb-input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+          <mdb-input label="Confirm your email" icon="exclamation-triangle" group type="text" validate error="wrong" success="right"/>
+          <mdb-input label="Your password" icon="lock" group type="password" validate/>
+        </div>
+        <div class="text-center py-4 mt-3">
+          <mdb-btn color="cyan" type="submit">Register</mdb-btn>
+        </div>
+      </form>
+    </mdb-card-body>
+  </mdb-card>
+  <!-- Card -->
 </template>
 
 <script>
 import auth from '../auth'
-
+import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbTextarea, mdbBtn, mdbIcon, mdbSelect, mdbCard, mdbCardBody } from 'mdbvue';
 export default {
+  name: "LoginComponent",
+  components: {
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+    mdbInput,
+    mdbTextarea,
+    mdbBtn,
+    mdbIcon,
+    mdbSelect,
+    mdbCard,
+    mdbCardBody
+  },
   data () {
     return {
       email: '',
