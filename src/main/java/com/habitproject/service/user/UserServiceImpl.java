9db93296589f66cd04bf5c93353ef2628d8dd35a
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-06-18 18:54:59 +0200

    c122.3 - reset: cleanup

--- src/main/java/com/habitproject/service/user/UserServiceImpl.java
+++ src/main/java/com/habitproject/service/user/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.habitproject.service.user;
 
-import com.habitproject.web.user.UserRequestModel;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 import java.util.Map;
