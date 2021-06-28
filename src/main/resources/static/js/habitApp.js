24a7993d1b141af9f6658fe3447aef5eba524a4b
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-06-28 14:45:22 +0200

    c172.0 - implementing new Version of cards

--- src/main/resources/static/js/habitApp.js
+++ src/main/resources/static/js/habitApp.js
@@ -2,118 +2,143 @@ const app = Vue.createApp({});
 
 app.component('habit_component', {
     template:
-        `         
+        `
           <!-- new habit form-->
           <div class="container-fluid">
-    
-              <!-- new habit form -->
-              <div class="card habits-form-card">
-                <div class="card-body">
-                   <form id="newHabitForm">
-                     <div class = "row align-items-center justify-content-center">
-                        
-                       <!-- tag input -->
-                        <div class="col-lg-4">
-                          <label class="form-label" for="newHabitTag" style>My new habit is:</label>
-                          <input v-model="newHabitTag" type="text" id="newHabitTag" class="form-control mr-sm-2" />
-                        </div>
-                        <!-- tag input -->
-                        
-                       <!-- frequency input -->
-                       <div class="col-lg-3">
-                         <label class="form-label" for="newHabitFrequency" style>I want to do it:</label>
-                         <select v-model="newHabitFrequency" id="newHabitFrequency" class="form-select mr-sm-2">
-                           <option value="daily">daily</option>
-                           <option value="weekly">weekly</option>
-                           <option value="biweekly">biweekly</option>
-                           <option value="triweekly">triweekly</option>
-                           <option value="monthly">monthly</option>
-                         </select>
-                       </div>
-                       <!-- frequency input -->
-                        
-                       <!-- quantity input -->
-                        <div class="col-lg-2">
-                          <label class="form-label" for="newHabitQuantity" style>In a frequency of:</label>
-                          <input v-model="newHabitQuantity" type="number" id="newHabitQuantity" class="form-control mr-sm-2" />
-                        </div>
-                        <!-- quantity input -->
-                       
-                       <!-- icon selection -->
-                       <div class="col-lg-2">
-                         <label class="form-label" for="newHabitIcon" style>Select your icon</label>
-                         <select v-model="newHabitIcon" id="newHabitIcon" class="form-select mr-sm-2">
-                           <option value="none"> no icon</option>
-                           <option value="sport">sport</option>
-                           <option value="study">study</option>
-                           <option value="community">community</option>
-                           <option value="mindfulness">mindfulness</option>
-                         </select>
-                       </div>
-                       <!-- icon selection -->
-                       
-                       <!-- submit button -->
-                        <div class="col-lg-1">
-                          <label class="form-label"></label>
-                            <button type="submit" class="btn btn-block habit-button" v-on:click="putHabit()" style="height: 3rem !important">
-                              CREATE
-                            </button>
-                        </div>
-                        <!-- submit button -->
-                    </div>
-                   </form> 
-                  <!-- new habit form -->
+
+          <!-- new habit form -->
+          <div class="card habits-form-card">
+            <div class="card-body">
+              <form id="newHabitForm">
+                <div class="row align-items-center justify-content-center">
+
+                  <!-- tag input -->
+                  <div class="col-lg-4">
+                    <label class="form-label" for="newHabitTag" style>My new habit is:</label>
+                    <input v-model="newHabitTag" type="text" id="newHabitTag" class="form-control mr-sm-2"/>
+                  </div>
+                  <!-- tag input -->
+
+                  <!-- frequency input -->
+                  <div class="col-lg-2">
+                    <label class="form-label" for="newHabitFrequency" style>I want to do it:</label>
+                    <select v-model="newHabitFrequency" id="newHabitFrequency" class="form-select mr-sm-2">
+                      <option value="daily">daily</option>
+                      <option value="weekly">weekly</option>
+                      <option value="biweekly">biweekly</option>
+                      <option value="triweekly">triweekly</option>
+                      <option value="monthly">monthly</option>
+                    </select>
+                  </div>
+                  <!-- frequency input -->
+
+                  <!-- quantity input -->
+                  <div class="col-lg-2">
+                    <label class="form-label" for="newHabitQuantity" style>In a frequency of:</label>
+                    <input v-model="newHabitQuantity" type="number" id="newHabitQuantity" class="form-control mr-sm-2"/>
+                  </div>
+                  <!-- quantity input -->
+
+                  <!-- icon selection -->
+                  <div class="col-lg-2">
+                    <label class="form-label" for="newHabitIcon" style>Select your icon</label>
+                    <select v-model="newHabitIcon" id="newHabitIcon" class="form-select mr-sm-2">
+                      <option value="none"> no icon</option>
+                      <option value="sport">sport</option>
+                      <option value="study">study</option>
+                      <option value="community">community</option>
+                      <option value="mindfulness">mindfulness</option>
+                    </select>
+                  </div>
+                  <!-- icon selection -->
+
+                  <!-- submit button -->
+                  <div class="col-lg-1">
+                    <label class="form-label"></label>
+                    <button type="submit" class="btn btn-rounded habit-button" v-on:click="createHabit()"
+                            style="height: 3rem !important">
+                      CREATE
+                    </button>
+                  </div>
+                  <!-- submit button -->
                 </div>
-              </div>
+              </form>
+              <!-- new habit form -->
+            </div>
+          </div>
 
-              <!-- habits cards -->
-              <div class="row" style="padding-top: 5%">
-                <div class="card habits-list-card border" v-for="habit in habits">
-                  <div class="card-body">
-                    <div class="card-title card-title-m roboto-bold">
-                      {{ habit.tag }} 
-                    </div>
-                    <div class="card-text">
-                      <div class="row justify-content-between">
+          <!-- habits cards -->
+          <div class="row" style="padding-top: 5%">
+            <div class="card habits-list-card border" v-for="habit in habits" :done='false'>
+
+              <div class="card-body">
+                <!--card title-->
+                <div class="card-title card-title-m roboto-bold">
+                  {{ habit.tag }}
+                </div>
+                <!--card title-->
+                <!--card text-->
+                <div class="card-text">
+                      <!--habit information & buuttons row-->
+                      <div class="row justify-content-between align-items-center">
                         
-                        <div class="col-md-6">
-                            <form disabled id="habit-display-{{habit.hid}}">
-                              <div class="row justify-content-start">
-                                <div class="col-lg-4">
-                                  <input v-model="updateHabitTag" :placeholder="[[habit.tag]]" type="text" id="updateHabitTag" class="form-control habit-display-form mr-sm-2"/>
-                                </div>
-                                <div class="col-lg-4">
-                                  <input v-model="updateHabitFrequency" :placeholder="[[habit.frequency]]"type="text" id="updateHabitTag" class="form-control habit-display-form mr-sm-2"/>
-                                </div>
-                                <div class="col-lg-4">
-                                  <input v-model="newHabitQuantity" :placeholder="[[habit.quantity]]" type="number" id="newHabitQuantity" class="form-control mr-sm-2 habit-display-form"/>
-                                </div>
-                              </div>
-                            </form>
+                        <div class="col-md-9 roboto-medium">
+                            <div class="habit-display-text"> Your doing &nbsp</div>
+                            <div class="habit-display-text roboto-black vue-input-style"> {{ habit.tag }} &nbsp </div>
+                            <div class="habit-display-text"> in a &nbsp </div>
+                            <div class="habit-display-text roboto-black vue-input-style "> {{ habit.frequency }} &nbsp </div>
+                            <div class="habit-display-text"> frequency, &nbsp </div>
+                            <div class="habit-display-text roboto-black vue-input-style"> {{ habit.quantity }} &nbsp </div>
+                            <div class="habit-display-text"> time(s) a period.</div>
                         </div>
-                        
-                        <div class="col-md-3" id="save-edit-button">
-                          <button type="button" class="btn btn-floating edit-btns" style="visibility: hidden" onclick="saveEditing(habit.hid)">
-                            <i class="fas fa-save"></i>
-                          </button>
 
-                          <button type="button" id="edit-button " class="btn btn-floating edit-btns" onclick="toggleEditing(habit.hid)">
-                            <i class="fas fa-pen"></i>
-                          </button>
+                        <div class="col-md-3">
+                          <button type="button" id="save-edit-button-[[habit.hid]]" class="btn btn-floating edit-btns" style="visibility: hidden" v-on:click="saveEditing(habit.hid)">
+                            <i class="fas fa-save"></i></button>
 
-                          <button type="button" id="done-button" class="btn btn-rounded habit-button" style="margin-left: 3rem !important;" onclick="doneHabit(habit.hid)">
-                            DONE
-                          </button>
+                          <button type="button" id="edit-button " class="btn btn-floating edit-btns" v-on:onclick="toggleEditing(habit.hid)">
+                            <i class="fas fa-pen"></i></button>
+
+                          <button type="button" id="done-button" class="btn btn-rounded habit-button" style="margin-left: 1rem !important;" v-on:click="doneHabit(habit.hid)">
+                            DONE</button>
                         </div>
-                        
                       </div>
+                      <!--habit information row-->
+
+                      <!--update habit form row-->
+                      <div class="row justify-content-start" id="habit-display-edit-form-[[habit.hid]]" style="visibility:collapse; display:none">
+                        <form>
+                        
+                          <div class="col-md-5">
+                            <input v-model="updateHabitTag" :placeholder="[[habit.tag]]" type="text" id="updateHabitTag" class="form-control habit-display-form mr-sm-2"/>
+                          </div>
+
+                          <div class="col-md-3">
+                            <select v-model="updateHabitFrequency" id="updateHabitFrequency" class="form-select mr-sm-2 habit-display-form">
+                              <option value="daily">daily</option>
+                              <option value="weekly">weekly</option>
+                              <option value="biweekly">biweekly</option>
+                              <option value="triweekly">triweekly</option>
+                              <option value="monthly">monthly</option>
+                            </select>
+                          </div>
+
+                          <div class="col-md-3">
+                            <input v-model="newHabitQuantity" :placeholder="[[habit.quantity]]+' times'" type="number"
+                                   id="newHabitQuantity" class="form-control mr-sm-2 habit-display-form"/>
+                          </div>
+                        </form>
+                      </div>
+                      <!--update habit form row-->
                     </div>
+                    <!--habit information and form col-->
                   </div>
                 </div>
+                <!--card text-->
               </div>
-              <!-- habits cards -->
-          </div>
-    `,
+            </div>
+            <!-- habits cards -->
+        `,
     data() {
         let uid;
         return {
@@ -137,7 +162,8 @@ app.component('habit_component', {
                 frequency: this.newHabitFrequency,
                 lastCheck: LocalDateTime.now(),
                 quantity: this.newHabitQuantity,
-                tag: this.newHabitTag
+                tag: this.newHabitTag,
+                uid: this.uid
             })
                 .then(response => {
                     result = response.data; console.log("new habit created successfully")
@@ -154,23 +180,25 @@ app.component('habit_component', {
         },
 
         doneHabit(hidInput){
-            const habit = habits.find(element => element.hid = hidInput);
+            const habit = this.habits.find(element => element.hid = hidInput);
+            const URL = '/habits/' + habit.hid.toString();
 
-            axios.put('/habits/' + hidInput.toString, {
-                done: true,
-                doneAmount: habit.doneAmount + 1,
+            console.log(URL)
+            console.log(habit)
+            console.log(hidInput)
+
+            axios.put(URL, {
+                done: habit.done,
+                doneAmount: habit.doneAmount+1,
                 frequency: habit.frequency,
-                lastCheck: LocalDateTime.now(),
                 quantity: habit.quantity,
-                tag: habit.tag
+                tag: habit.tag,
+                uid: habit.uid
             })
                 .then(response => {
-                    result = response.data;
-                    console.log("habit successfully marked as done")
+                    console.log("habit successfully marked as done");
+                    this.requestHabits(this.uid);
                 });
-
-            this.requestHabits(this.uid);
-
         },
 
         updateHabit(hidInput){
@@ -180,36 +208,38 @@ app.component('habit_component', {
                 done: habit.done,
                 doneAmount: habit.doneAmount,
                 frequency: updateFrequency,
-                lastCheck: habit.lastCheck,
                 quantity: updateQuantity,
-                tag: updateTag
+                tag: updateTag,
+                uid: this.uid
             })
                 .then(response => {
                     result = response.data;
-                    console.log("habit updated successfully")
+                    console.log("habit updated successfully");
+                    this.requestHabits(this.uid);
                 });
 
-            this.requestHabits(this.uid);
-
-        },
-
-        toggleEditing(hidInput){
-            const habitDisplay = document.getElementById("habit-display"+hidInput.toString());
-            habitDisplay.toggleAttribute("disabled");
-            if(habitDisplay.style.outline !== "var(--habit-color)"){
-                habitDisplay.style.outline = "var(--habit-color)";
-            }else {habitDisplay.style.outline = "none !important";}
-            this.toggleSaveButton();
         },
 
         saveEditing(hidInput){
-            const habitDisplay = document.getElementById("habit-display"+hidInput.toString());
-            habitDisplay.toggleAttribute("disabled");
             this.updateHabit(hidInput);
+            this.toggleEditing(hidInput);
+            this.toggleSaveButton(hidInput);
         },
 
-        toggleSaveButton(){
-            const btn = document.getElementById("save-edit-button");
+        toggleEditing(hidInput){
+            const habitEditForm = document.getElementById("habit-display-edit-form-"+hidInput.toString());
+            if (habitEditForm.style.visibility === "collapsed"){
+                habitEditForm.style.visibility = "visible";
+                habitEditForm.style.display = "block";
+            } else {
+                habitEditForm.style.display = "collapsed";
+                habitEditForm.style.display= "none";
+            }
+            this.toggleSaveButton();
+        },
+
+        toggleSaveButton(hidInput){
+            const btn = document.getElementById("save-edit-button-"+hidInput.toString());
             if (btn.style.visibility === "hidden"){
                 btn.style.display = "visible";
             } else {btn.style.display = "hidden";}
