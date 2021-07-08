fb90cca554af8b4c5d2ba58acd73cab174184931
LennardZuendorf <l.p.zuendorf@gmail.com>
2021-07-08 22:31:58 +0200

    c185.0 - implementing full user data output

--- src/main/java/com/habitproject/web/user/UserRESTController.java
+++ src/main/java/com/habitproject/web/user/UserRESTController.java
@@ -7,12 +7,14 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 class UserRESTController {
 
     @GetMapping("/user")
-    ResponseEntity <Object> getUserId (@AuthenticationPrincipal OidcUser user) {
+    ResponseEntity <Map<String, Object>> getUserId (@AuthenticationPrincipal OidcUser user) {
         System.out.println(user.getAttributes().get("sub"));
-        return ResponseEntity.status(HttpStatus.OK).body(user.getAttributes().get("sub"));
+        return ResponseEntity.status(HttpStatus.OK).body(user.getAttributes());
     }
 }
