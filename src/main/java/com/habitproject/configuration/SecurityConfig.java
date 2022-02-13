c8f51f36a14cacfea496ad917309d04359b6b0ce
LennardZuendorf <l.p.zuendorf@gmail.com>
2022-02-13 15:42:49 +0100

    c185.3 - switching from okta to auth0

--- src/main/java/com/habitproject/configuration/SecurityConfig.java
+++ src/main/java/com/habitproject/configuration/SecurityConfig.java
@@ -1,54 +1,32 @@
 package com.habitproject.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-
-import java.net.URI;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    ClientRegistrationRepository clientRegistrationRepository;
-
-    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri(URI.create("https://habit-project.herokuapp.com/"));
-        return successHandler;
-    }
-
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            //public endpoints
+            .antMatchers(
+                    Endpoints.Site.INDEX,
+                    Endpoints.Site.SLASH,
+                    Endpoints.Site.LOGIN
+            ).permitAll()
+            // static resources
+            .antMatchers(
+                    "/css/**",
+                    "/img/**",
+                    "/js/**",
+                    "./templates"
+            ).permitAll()
 
-                // public pages
-                .antMatchers(
-                        Endpoints.Site.INDEX,
-                        Endpoints.Site.SLASH,
-                        Endpoints.Site.LOGIN,
-                        Endpoints.Site.DASH
-                ).permitAll()
-                // static resources
-                .antMatchers(
-                        "/css/**",
-                        "/img/**",
-                        "/js/**"
-                ).permitAll()
-                .anyRequest().authenticated()
+            .anyRequest().authenticated()
 
-                // send the user back to the root page when they logout
-                .and().logout().logoutSuccessHandler(oidcLogoutSuccessHandler())
-
-                .and().oauth2Client()
-                .and().oauth2Login().loginPage("/login");
+            .and().oauth2Login()
+            .and().logout();
     }
-
 }
