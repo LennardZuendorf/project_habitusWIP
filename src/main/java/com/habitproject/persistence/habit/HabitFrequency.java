46765d049e026c1e17dce0a77230ea828102aad5
LennardZuendorf <l.p.zuendorf@gmail.com>
2022-05-28 19:38:19 +0200

    deleted old files

--- src/main/java/com/habitproject/persistence/habit/HabitFrequency.java
+++
@@ -1,9 +0,0 @@
-package com.habitproject.persistence.habit;
-
-/**
- * HabitFrequency is the enum defining the 5 possible frequency of a habit
- */
-public enum HabitFrequency {
-
-    DAILY, WEEKLY, BIWEEKLY, TRIWEEKLY, MONTHLY
-}
